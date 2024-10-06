d082d2dd1719dd5f998de6a03e813c1fdbc5e8e3
Kpyr41 <kpyr41@gmail.com>
2024-10-06 14:38:03 +0530

    added server api link

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   
-  API_BASE_URL: 'http://localhost:8080/api/',
-
+  // API_BASE_URL: 'http://localhost:8080/api/',
+  API_BASE_URL: 'http://api.kpyr.in/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://localhost:8080/api/',
-
+  // API_BASE_URL: 'http://localhost:8080/api/',
+  API_BASE_URL: 'http://api.kpyr.in/api/',
 };
